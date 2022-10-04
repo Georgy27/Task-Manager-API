4321fefe3d5553ae7e8eacf3d3120331d53b36fd
Georgy <Aegoraa@yandex.ru>
2022-10-04 21:43:19 +0300

    first commit

---
+++ controllers/tasks.js
@@ -0,0 +1,30 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  return res.send("get all tasks");
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  return res.status(201).json({ task });
+};
+
+const getTask = (req, res) => {
+  return res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  return res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  return res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
